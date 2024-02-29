75afa1ac74d962581142cdebc887df054afa4860
陈恩泉 <chenenquan@chenenquandeMacBook-Air.local>
2024-03-01 01:51:52 +0800

    init

--- gunicorn.conf.py
+++
@@ -1,5 +0,0 @@
-bind="0.0.0.0:5000"
-worker_class="gthread"
-workers=4
-threads=10
-timeout=3600
--- worker.py
+++
@@ -1,6 +0,0 @@
-from langchain_community.document_loaders import PyPDFLoader
-
-loader = PyPDFLoader("./test/Ethereum_Whitepaper_Buterin_2014.pdf")
-
-pages = loader.load_and_split()
-

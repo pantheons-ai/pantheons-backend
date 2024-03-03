7badb711a63e5c0017ff06258222ba43b6f25a5c
陈恩泉 <chenenquan@chenenquandeMacBook-Air.local>
2024-03-03 23:12:52 +0800

    add http service for story protocol

--- python/infersvr.py
+++ python/infersvr.py
@@ -2,14 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
-@app.route("/v1/ip", methods=["POST"])
-def create_ip():
-    # 1. Create folder in IPFS for this IP
-    # 2. Release smart contracts on the chain
-    pass
-
-
 @app.route("/v1/files", methods=["POST"])
 def upload_files():
     # upload files to service and then pushlish to IPFS asynchronously by worker
